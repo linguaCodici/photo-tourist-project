b09073935af0ab042779f2c893c059365c484212
linguaCodici <victoriqueee@gmail.com>
2018-11-24 22:52:43 -0600

    added mongo support

--- config/application.rb
+++ config/application.rb
@@ -29,6 +29,10 @@ module Myapp
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    # MongoDB config and default db setup
+    Mongoid.load!('./config/mongoid.yml')
+    config.generators {|g| g.orm :active_record}
+
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
